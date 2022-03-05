1f1d3d9b5e608ce2b77e68f9253c21bb630768dc
Eren Yusuf Duran <erenydurann@gmail.com>
2022-03-05 13:46:39 +0300

    firebaseSetups

---
+++ src/redux/firebase.js
@@ -0,0 +1,18 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDc-ukALVxqxRZBVbpQqsSnS04esTBLlN4",
+  authDomain: "react-firebase-auth-8a981.firebaseapp.com",
+  projectId: "react-firebase-auth-8a981",
+  storageBucket: "react-firebase-auth-8a981.appspot.com",
+  messagingSenderId: "1007941453460",
+  appId: "1:1007941453460:web:17cd13523c0ee828689681",
+};
+
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
+const facebookAuthProvider = new firebase.auth.FacebookAuthProvider();
+
+export { auth, googleAuthProvider, facebookAuthProvider };
