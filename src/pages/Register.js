395874da096f9c089341902da1d75ecd13a2e776
Eren Yusuf Duran <erenydurann@gmail.com>
2022-03-05 14:39:58 +0300

    registerPage

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,9 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory, Link } from "react-router-dom";
+import "./Register.css";
 
 const Register = () => {
+  const [state, setState] = useState({
+    displayName: "",
+    email: "",
+    password: "",
+    passwordConfirm: "",
+  });
+  const { email, password, displayName, passwordConfirm } = state;
+  const handleSubmit = () => {};
+  const handleChange = () => {};
   return (
     <div>
-      <h2>Register</h2>
+      <div id="register-form">
+        <form className="form-signup" onSubmit={handleSubmit}>
+          <h1
+            className="h3 mb-3 font-weight-normal"
+            style={{ textAlign: "center" }}
+          >
+            SIGN UP
+          </h1>
+          <input
+            type="text"
+            id="displayName"
+            className="form-control"
+            placeholder="Full Name"
+            name="displayName"
+            onChange={handleChange}
+            value={displayName}
+            required
+          />
+          <input
+            type="email"
+            id="userEmail"
+            className="form-control"
+            placeholder="Email Address"
+            name="email"
+            onChange={handleChange}
+            value={email}
+            required
+          />
+          <input
+            type="password"
+            id="inputPassword"
+            className="form-control"
+            placeholder="Password"
+            name="password"
+            onChange={handleChange}
+            value={password}
+            required
+          />
+          <input
+            type="password"
+            id="passwordConfirm"
+            className="form-control"
+            placeholder="Repeat Password"
+            name="passwordConfirm"
+            onChange={handleChange}
+            value={passwordConfirm}
+            required
+          />
+          <button className="btn btn-primary btn-block" type="submit">
+            <i className="fas fa-user-plus"></i> Sign Up
+          </button>
+            <Link to="/login">
+                <i className="fas fa-angle-left"></i> Back to Login
+                </Link>
+        </form>
+      </div>
     </div>
   );
 };
