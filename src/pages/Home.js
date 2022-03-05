659cf2edd2393b83ff79eb0c90ef7471310e8913
Eren Yusuf Duran <erenydurann@gmail.com>
2022-03-05 15:04:35 +0300

    reducerUpdate

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,9 @@ import React from "react";
 const Home = () => {
   return (
     <div>
-      <h2>Home</h2>
+      <h2>Welcome to Our Site</h2>
+      <br />
+      <button className="btn btn-danger">Logout</button>
     </div>
   );
 };
